a93394033a4329424483a0d05e9e9ba27d2f7ab5
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2022-08-25 20:28:45 +0530

    Delete test.py

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from cv2 import createTonemap
-from boxdb import *
-
-# create_project({'name':'kshitij'})
-# create_column('kshitij',['task','data','status'])
-# add_row('kshitij',['help','22-07-22','False'])
-get_table('soham')
